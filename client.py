2e08414cfd9cada2e3bec54740e00a13bf50c601
Tanaphol <tanapholw58@email.nu.ac.th git config --global user.name Your>
2018-01-23 22:08:16 +0700

    PIC

---
+++ client.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import socket
+import base64
+
+file = "Pic.jpg"
+bytes1 = open(file,'rb').read() #ÕË“π‰ø≈Ï‰∫µÏ
+bytes1 = base64.b64encode(bytes1) #‡¢È“√À—  base64
+
+TCP_IP = '127.0.0.1'
+TCP_PORT = 5011
+BUFFER_SIZE = 1024
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((TCP_IP, TCP_PORT))
+s.send(bytes1)
+data = s.recv(BUFFER_SIZE)
+s.close()
+
+print "received data:", data
---
+++ server.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import socket
+import base64
+import os
+
+
+
+TCP_IP = '127.0.0.1'
+TCP_PORT = 5011
+BUFFER_SIZE = 1024000
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((TCP_IP, TCP_PORT))
+s.listen(1)
+
+conn, addr = s.accept()
+os.mkdir('image_from_client')
+myfile = open('image_from_client/ABC.jpg', 'wb') #‡ª‘¥‰ø≈Ï·≈–‡¢’¬π‰ø≈Ï
+print 'Connection address:', addr
+while 1:
+    data = conn.recv(BUFFER_SIZE)
+    if not data: break
+    data = base64.b64decode(data) #∂Õ¥√À—  base64
+    myfile.write(data) #‡¢È“¢ÈÕ¡Ÿ≈≈ß‰ø≈Ï
+    myfile.close() #ª‘¥‰ø≈Ï
+    
+conn.close()
